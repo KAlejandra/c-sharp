4e2e36e26fa4abd7642447b8567fa694a318be3f
Karol Giron <kbenites4518@gmail.com>
2022-06-28 17:25:34 -0600

    Agg

---
+++ Ejercicios/inventario/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace holamundo
+{
+    class Program
+    {
+        static void Main (string [] args) {
+
+            int opcion = 0;
+        
+            while (true)
+            {
+                Console.Clear ();
+                Console.WriteLine(" Sistema de Inventario");
+                Console.WriteLine("***********************");
+                Console.WriteLine("");
+                Console.WriteLine("1 - Productos");
+                Console.WriteLine("2 - Ingreso de Inventario");
+                Console.WriteLine("3 - Salida de inventario");
+                Console.WriteLine("0 - Salir");
+                opcion = Console.Read();
+
+                switch (opcion)
+                {
+                    case 1:
+                    Console.WriteLine("Productos!");
+                    Console.ReadLine();
+                    break;
+
+                    default:
+                }
+                if (opcion == 0)
+                {
+                    break; 
+                }
+            }
+
+            
+        }
+    }
+}
