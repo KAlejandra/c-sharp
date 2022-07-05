df12585a107b3ff818961687362eac2f1b1eeea8
Karol Giron <kbenites4518@gmail.com>
2022-07-04 22:45:10 -0600

    Agg funciones

--- Ejercicios/5 - Propiedades/propiedades/Alumno.cs
+++ Ejercicios/5 - Propiedades/propiedades/Alumno.cs
@@ -1,13 +1,16 @@
 public class Alumno
 {
-   public int Id { get; set; } 
-   public string PrimerNombre { get; set; }
-   public string SegundoNombre { get; set; }
-
-   public Alumno () {
-        Id = 1;
-   }
-   public Alumno (int id) {
-        Id = id;
-   }
+     public int Id { get; set; } 
+     public string PrimerNombre { get; set; }
+     public string SegundoNombre { get; set; }
+     public Alumno() { 
+          Id = 1;
+     }
+     public Alumno (int id) {
+          Id = id;
+     }
+     public Alumno (string primerNombre, string segundoNombre) {
+          PrimerNombre = primerNombre;
+          SegundoNombre = segundoNombre;
+     }
 }
--- Ejercicios/5 - Propiedades/propiedades/Program.cs
+++ Ejercicios/5 - Propiedades/propiedades/Program.cs
@@ -18,10 +18,13 @@ namespace propiedades
             Alumno c = new Alumno (3);
             c.PrimerNombre = "Pedro";
             c.SegundoNombre = "Perez";
+
+             Alumno d = new Alumno ("Juan", "Jimenez");
         
             Console.WriteLine(a.Id);
             Console.WriteLine(b.Id);
             Console.WriteLine(c.Id);
+            Console.WriteLine(d.PrimerNombre + " " + d.SegundoNombre);
         }
     }
 }
