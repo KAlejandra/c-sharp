ad3e293d5acc50c05e2aec9dda73e1cd0903271f
Karol A. Benites <98500847+KAlejandra@users.noreply.github.com>
2022-08-02 18:17:48 -0600

    Update Program.cs

--- Pilares/Program.cs
+++ Pilares/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 
-namespace _12_ejercicio_clase_animales
+namespace Pilares
 {
     class Program
     {
@@ -23,10 +23,64 @@ namespace _12_ejercicio_clase_animales
             Gato g = new Gato();
             g.Nombre = "Garfield";
             g.Patas = 4;
-            g.Pelaje = "Peludo";
             g.Comer();
             g.Caminar();
             g.Maullar();
+
+            Console.WriteLine(g.Nombre);
+            Console.WriteLine(g.Patas);
+
+            mono m = new mono();
+            m.Nombre = "Miki";
+            m.Patas = 4;
+            m.Comer();
+            m.Caminar();
+            m.Maullar();
+
+            Console.WriteLine(m.Nombre);
+            Console.WriteLine(m.Patas);
+
+            Aguila Aa = new Aguila();
+            Aa.Nombre = "Velss";
+            Aa.ColordePlumaje = 2;
+            Aa.Comer();
+            Aa.VolarAlto();
+            Aa.Generalidades();
+
+            Console.WriteLine(Aa.Nombre);
+            Console.WriteLine(Aa.ColordePlumaje);
+
+            Loro L = new Loro();
+            L.Nombre = "Lorencio";
+            L.ColordePlumaje= 3;
+            L.Comer();
+            L.Volar();
+            L.Generalidades();
+
+            Console.WriteLine(L.Nombre);
+            Console.WriteLine(L.ColordePlumaje);
+
+            PezGlobo G = new PezGlobo();
+            G.Nombre = "Lorencio";
+            G.NumerodeAletas= 2;
+            G.Comer();
+            G.Nadar();
+            G.Generalidades();
+
+            Console.WriteLine(G.Nombre);
+            Console.WriteLine(G.NumerodeAletas);
+
+            PezPayaso P = new PezPayaso();
+            P.Nombre = "Lorencio";
+            P.NumerodeAletas= 2;
+            P.Describir();
+            P.Nadar();
+            P.Generalidades();
+
+            Console.WriteLine(P.Nombre);
+            Console.WriteLine(P.NumerodeAletas);
+
+
         }
     }
 }
